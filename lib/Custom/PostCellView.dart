9e9dc9c40d543e186cb55e861803b0ed8c64c2f2
arion0501 <targvi@gmail.com>
2023-10-08 17:42:38 +0200

    Commit Atasco Clase Solucionado

---
+++ lib/Custom/PostCellView.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class PostCellView extends StatelessWidget{
+
+  final String sText;
+  final int iColorCode;
+  final double dFontSize;
+
+  const PostCellView ({
+    required this.sText,
+    required this.iColorCode,
+    required this.dFontSize});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amber[iColorCode],
+      child: Row(
+        children: [
+          Image.asset('resources/logo_kyty.png',
+            width: 30,
+            height: 45),
+          Text(sText)
+        ],
+      )
+    );
+  }
+
+}
--- lib/Main/HomeView.dart
+++ lib/Main/HomeView.dart
@@ -1,5 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:kyty/Custom/PostCellView.dart';
+import 'package:kyty/FirestoreObjects/FbPost.dart';
 
 class HomeView extends StatefulWidget {
   @override
@@ -7,37 +9,62 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  late BuildContext _context;
+
+  FirebaseFirestore db = FirebaseFirestore.instance;
+
+  final List<FbPost> posts = [];
 
   @override
   void initState() {
     super.initState();
+    descargarPosts();
   }
 
-  void onClickVolver(){
-    FirebaseAuth.instance.signOut();
-    Navigator.of(_context).popAndPushNamed('/loginview');
+  void descargarPosts() async {
+    CollectionReference<FbPost> collection = db.collection("Post")
+        .withConverter(fromFirestore: FbPost.fromFirestore,
+        toFirestore: (FbPost post, _) => post.toFirestore());
+
+    QuerySnapshot<FbPost> querySnapshot = await collection.get();
+    for(int i = 0; i < querySnapshot.docs.length; i++){
+      setState(() {
+        posts.add(querySnapshot.docs[i].data());
+      });
+    }
   }
 
-  final List<String> posts = <String> ['A, B, C'];
-  final List <int> colorCode = <int> [500, 600, 100];
+  final List <String> postsOLD = <String> ['A', 'B', 'C'];
+  final List <int> colorCode = <int> [600, 500, 100];
   final List <double> fontSize = <double> [30, 15, 70];
-
-  Widget? creadorDeItemLista(BuildContext contect, int index) {
-    return Text('Post' + posts[index],
-    style: TextStyle(color: Colors.pinkAccent[colorCode[index]],
-    fontSize: fontSize[index]));
-  }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(title: Text("Kyty"),),
-      body: ListView.builder(
+      body: ListView.separated(
+        padding: EdgeInsets.all(8),
         itemCount: posts.length,
         itemBuilder: creadorDeItemLista,
+        separatorBuilder: creadorDeSeparadorLista,
       ),
+    );
+  }
+
+  Widget? creadorDeItemLista(BuildContext context, int index) {
+    return PostCellView(sText: posts[index].titulo,
+        iColorCode: 0,
+        dFontSize: 20
+    );
+  }
+
+  Widget creadorDeSeparadorLista(BuildContext context, int index) {
+    // return Divider(thickness: 5);
+    return Column(
+        children: [
+          Divider(),
+          // CircularProgressIndicator(),
+          // Image.network('resources/logo_kyty.png')
+        ]
     );
   }
 }
