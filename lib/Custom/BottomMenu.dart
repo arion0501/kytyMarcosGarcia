d2026af4d1b78a22fae12572df864bfebb71be14
Marcos García <markarules01@gmail.com>
2023-11-28 12:52:40 +0100

    Commit Solución Error Imagen en Navigator Menú (Fondo) por la resolución

--- lib/Custom/BottomMenu.dart
+++ lib/Custom/BottomMenu.dart
@@ -14,7 +14,7 @@ class BottomMenu extends StatelessWidget{
         children: [
           TextButton(onPressed: () => evento!(0), child: Icon(Icons.list,color: Colors.pink,)),
           TextButton(onPressed: () => evento!(1), child: Icon(Icons.grid_view,color: Colors.pink,)),
-          IconButton(onPressed: () => evento!(2), icon: Image.asset("resources/logo_kyty.png"))
+          IconButton(onPressed: () => evento!(2), icon: Image.asset("resources/logo_kyty.png", width: 30, height: 30))
         ]
     );
   }
